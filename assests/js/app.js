71a0dd6f3b8cdf61000c8905778236e85757d740
Omar Ibrahim <omaribrahim@Omars-MacBook-Air-2.local>
2024-01-09 00:12:01 +0000

    fixed style and local storge

--- assests/js/app.js
+++ assests/js/app.js
@@ -1,5 +1,5 @@
 const api = "fa0b6ea4253b5b114a91a3a4bf1293ec";
-
+    var forecastContainer = document.getElementById("forcast-container");
 class Forecast{
   constructor (date,icon,temp,wind,humidity){
     this.date = date;
@@ -9,10 +9,16 @@ class Forecast{
     this.humidity = humidity;
   }
 }
+// $(".current-weather").hide();
 
-$('#search-button').on('click', function (event) {
-  event.preventDefault()
+$('#search-button').on('click',function(e){
+  e.preventDefault()
   var cityLocation = $('#search-input').val();
+  searchAPI(cityLocation)
+  $(".current-weather").show();
+} );
+function searchAPI(cityLocation) {
+
   var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityLocation + "&appid=" + api+"&units=metric";
 
 
@@ -25,14 +31,25 @@ $('#search-button').on('click', function (event) {
 
       console.log(data)
 
-     var date = data.list[0].dt_txt.split(' ')[0]
-     var icon = data.list[0].weather[0].icon
-     var temp = data.list[0].main.temp
-     var wind = data.list[0].wind.speed
-     var humidity = data.list[0].main.humidity
-    
+     var currentdate = data.list[0].dt_txt.split(' ')[0]
+     var currentIcon = data.list[0].weather[0].icon
+     var currentTemp = data.list[0].main.temp
+     var currentWind = data.list[0].wind.speed
+     var currentHumidity = data.list[0].main.humidity
+     var currentCity = data.city.name
+
+    console.log(currentCity)
+
+    document.getElementById("date").innerHTML=currentCity +" ("+currentdate+")"+ `<img src="http://openweathermap.org/img/wn/${currentIcon}.png" alt="Weather Icon">`
+    document.getElementById("divTemp").textContent="Temp: "+currentTemp+" °C";
+    document.getElementById("divWind").textContent="Wind Speed: "+currentWind+" KPH";
+    document.getElementById("divHumidity").textContent="Humidity: "+currentHumidity+" %"
+
+
+
+    forecastContainer.innerHTML = ""
      var forecasts = [];
-     for(let i=0;i<data.list.length;i++){
+     for(let i=7;i<data.list.length;i+=8){
       var date = data.list[i].dt_txt.split(' ')[0]
       var icon = data.list[i].weather[0].icon
       var temp = data.list[i].main.temp
@@ -41,19 +58,19 @@ $('#search-button').on('click', function (event) {
 
       forecasts.push(new Forecast(date,icon,temp,wind,humidity))
       var card = document.createElement("div");
-      card.className= "col-lg-4 col-12 mb-4 cd";
+      card.className= "col mw-15 mb-4 cd";
       card.innerHTML= `
       <div class= "card">
       <div class="card-body">
       <h2 class="card-title text">${date}</h2>
       <img src="http://openweathermap.org/img/wn/${icon}.png" alt="Weather Icon">
-      <p class="card-text text"> Temperature: ${temp}°C</p>
+      <p class="card-text text"> Temperature: ${temp} °C</p>
       <p class="card-text text"> Wind speed: ${wind} KPH</p>
-      <p class="card-text text"> Humidity: ${humidity}%</p>
+      <p class="card-text text"> Humidity: ${humidity} %</p>
       </div>
       </div>
       `;
-      var forecastContainer = document.getElementById("forcast-container");
+  
       if(forecastContainer){
         forecastContainer.appendChild(card)
       }
@@ -62,22 +79,18 @@ $('#search-button').on('click', function (event) {
 
 
     })
-});
-
+}
 let cityList = []
 function getInput() {
   $('#search-button').on('click', function (event) {
     event.preventDefault()
     const city = $('#search-input').val();
     // console.log(city)
-    cityList.push(city)
-    renderInput()
-    storeCityLsit()
+    storeCityLsit(city)
   })
 
 
 }
-getInput()
 function renderInput(city) {
   $('ul').empty()
   for (var i = 0; i < cityList.length; i++) {
@@ -86,34 +99,36 @@ function renderInput(city) {
 
     cityEL.on('click', function () {
       var cityName = $(this).text();
-      cityList.push(cityName);
-
-
+      storeCityLsit(cityName)
+      searchAPI(cityName)
      
     });
 
   }
 
-  storeCityLsit();
 
 }
-function storeCityLsit() {
+function storeCityLsit(cityName) {
+
+  if(!cityList.includes(cityName)){
+    cityList.push(cityName);
+    if(cityList.length>5){
+      cityList.shift()
+    }
+    }  
   localStorage.setItem('city-names', JSON.stringify(cityList));
+  renderInput()
+
 }
 
-// function loadCityList() {
-//   const storedCityList = localStorage.getItem('city-names');
-//   if (storedCityList) {
-//     cityList = JSON.parse(storedCityList);
-//   }
-// }
+function loadCityList() {
+  const storedCityList = localStorage.getItem('city-names');
+  if (storedCityList) {
+    cityList = JSON.parse(storedCityList);
+  }
+}
 
-// Load the city list on page load
-// loadCityList();
+loadCityList();
 
-// Initialize the event listener for the search button
 getInput();
-// dsiplay city names when submit box is clicked
-// grab city name from submit
-// have an array where we can push the city name in
-// grab all city name to add to elements in page
+
